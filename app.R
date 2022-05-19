7394946c9bd3fd66fb342fdb7b928f6448b614b0
Adam J Campbell <campbead@gmail.com>
2022-05-20 06:01:50 +1200

    added observer to change tab

--- app.R
+++ app.R
@@ -16,15 +16,14 @@ ui <- fluidPage(
       ),
       actionButton("search_action", "Search"),
       tabsetPanel(
-        id = "wizard",
+        id = "hidden_tab_panel",
         type = "hidden",
         tabPanel(
-          "first",
-          "hi"
+          "first"
         ),
         tabPanel(
           "second",
-          "sup",
+          hr(),
           dateInput(
             inputId = "start_date",
             label = "start date"
@@ -57,17 +56,23 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
+
+  observeEvent(
+    input$search_action,
+    updateTabsetPanel(inputId = "hidden_tab_panel", selected = "second")
+  )
+
   my_data <- eventReactive(input$search_action, {
     get_edit_history(input$text_input) %>%
       mutate(dates =  as_date(timestamp))
   })
+
 
   output$figure <- renderPlot(
     ggplot(my_data() %>% count(dates), aes(x = dates, n)) +
       geom_point(alpha = 0.1, color='darkblue') +
       geom_line(aes(y=rollmean(n, 14, na.pad=TRUE)), color="purple", size =1.5) +
       labs(y = "edit count") +
-      #lims(x = as.Date(c("2016-01-01", "2020-01-01")), y = c(0,50))+
       theme_minimal() +
       theme(axis.title.x = element_blank())
   )
