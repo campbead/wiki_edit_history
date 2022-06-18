d8d0c64b5d20875dba59a7c98c5f6c092b39fac6
Adam J Campbell <campbead@gmail.com>
2022-06-18 16:42:24 +1200

    various updates

--- app.R
+++ app.R
@@ -42,7 +42,8 @@ ui <- fluidPage(
         tabPanel(
           "second_main",
           addSpinner(
-            plotOutput("figure"), spin = "circle", color = "#E41A1C")
+            plotOutput("figure"), spin = "circle", color = "#E41A1C"
+          )
         )
       )
     )
@@ -55,28 +56,35 @@ server <- function(input, output) {
 
   observeEvent(
     input$search_action, {
- #     updateTabsetPanel(inputId = "hidden_tab_panel", selected = "second")
       updateTabsetPanel(inputId = "hidden_tab_panel_main", selected = "second_main")
+
+    },
+    once = TRUE,
+    priority = 10)
+
+  observeEvent(
+    length(my_data()) > 1, {
+      updateTabsetPanel(inputId = "hidden_tab_panel", selected = "second")
+    },
+    priority = 2,
+    once = TRUE
+    )
+
+
+
+  my_data <- eventReactive(
+    input$search_action,
+    {
+      get_edit_history(input$text_input) %>%
+        process_edits()
     })
-
-  # observeEvent(
-  #   my_data,{
-  #   if (length(my_data > 1)) {updateTabsetPanel(inputId = "hidden_tab_panel", selected = "second")}
-  #
-  # })
-
-  my_data <- eventReactive(input$search_action, {
-    get_edit_history(input$text_input) %>%
-      process_edits()
-      #mutate(dates =  as_date(timestamp))
-  })
 
 
 
   output$figure <- renderPlot(
     ggplot(my_data(), aes(x = dates, n)) +
       geom_point(alpha = 0.1, color='darkblue') +
-      #geom_line(aes(y=rollmean(n, 30, na.pad=TRUE)), color="purple", size =1.5) +
+      geom_line(aes(y=rollmean(n, 30, na.pad=TRUE)), color="purple", size =1.5) +
       labs(y = "edit count") +
       theme_minimal() +
       theme(axis.title.x = element_blank())
