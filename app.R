f3c4abb6ea9e7b9459963ca7b46f90cd2a27e473
Adam J Campbell <campbead@gmail.com>
2022-06-20 08:14:59 +1200

    removing un-need stuff

--- app.R
+++ app.R
@@ -10,41 +10,8 @@ source("functions.R")
 
 ui <- fluidPage(
   autoWaiter(),
-  h1("Wikipedia Edit History Viewer"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput(
-        "text_input",
-        "search"
-      ),
-      actionButton("search_action", "Search"),
-      tabsetPanel(
-        id = "hidden_tab_panel",
-        type = "hidden",
-        tabPanel(
-          "first"
-        ),
-        tabPanel(
-          "second",
-          hr(),
-          dateInput(
-            inputId = "start_date",
-            label = "start date"
-          )
-        )
-      ),
-    ),
-    mainPanel(
-      tabsetPanel(
-        id = "hidden_tab_panel_main",
-        type = "hidden",
-        tabPanel(
-          "second_main",
-          plotOutput("figure")
-        )
-      )
-    )
-  )
+  actionButton("search_action", "Run"),
+  textOutput("text")
 )
 
 
@@ -52,26 +19,15 @@ ui <- fluidPage(
 server <- function(input, output) {
 
 
-
-
-
-  my_data <- eventReactive(
-    input$search_action,
+  output$text <- renderText(
     {
-      get_edit_history(input$text_input) %>%
-        process_edits()
-    })
-
-
-
-  output$figure <- renderPlot(
-    ggplot(my_data(), aes(x = dates, n)) +
-      geom_point(alpha = 0.1, color='darkblue') +
-      geom_line(aes(y=rollmean(n, 30, na.pad=TRUE)), color="purple", size =1.5) +
-      labs(y = "edit count") +
-      theme_minimal() +
-      theme(axis.title.x = element_blank())
+      input$search_action
+      Sys.sleep(3)
+      "LONG CALCULATION"
+    }
   )
+
+
 }
 
 shinyApp(ui, server)
